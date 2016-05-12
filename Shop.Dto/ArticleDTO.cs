ebb91612e7069a7002e7156dd1370207412aeece
rsemann <semann.rafael@gmail.com>
2016-05-12 19:21:06 -0300

    DTO

---
+++ Shop.Dto/ArticleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Dto
+{
+    public class ArticleDTO
+    {
+        public int ArticleId { get; set; }
+
+        public string ArticleName { get; set; }
+
+        public string ArticleDescription { get; set; }
+    }
+}
