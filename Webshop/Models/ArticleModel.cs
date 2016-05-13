ee9d38b96c5eae92d360a666660622826d77ff40
rsemann <semann.rafael@gmail.com>
2016-05-13 01:33:30 -0300

    Model article

---
+++ Webshop/Models/ArticleModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models
+{
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Description { get; set; }
+
+        public float Value { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
