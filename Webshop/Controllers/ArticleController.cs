19611268c8fd78162060aa96c10e84eb47620b73
rsemann <semann.rafael@gmail.com>
2016-05-13 13:23:24 -0300

    Pagination

--- Webshop/Controllers/ArticleController.cs
+++ Webshop/Controllers/ArticleController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using Newtonsoft.Json;
 using Shop.Dto;
 using WebGrease.Css.Extensions;
@@ -15,13 +16,14 @@ namespace Webshop.Controllers
     public class ArticleController : Controller
     {
         // GET: Article
+        [HttpGet]
         public ActionResult Index()
         {
             var articles = new List<ArticleModel>();
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:61169/");
+                client.BaseAddress = new Uri(System.Configuration.ConfigurationManager.AppSettings["WebApiBaseAddress"]);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetAsync("api/article").Result;
@@ -40,7 +42,44 @@ namespace Webshop.Controllers
                     }));
                 }
             }
-            return View(articles);
+            
+            ViewBag.PageIndex = 0;
+            ViewBag.PageCount = articles.Count / 10;
+
+            return View(articles.Take(10));
+        }
+
+        [HttpPost]
+        public ActionResult Page(int pageIndex, int pageCount)
+        {
+            var articles = new List<ArticleModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(System.Configuration.ConfigurationManager.AppSettings["WebApiBaseAddress"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("api/article").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    jsonString.Wait();
+                    IEnumerable<ArticleDTO> articlesDto = JsonConvert.DeserializeObject<List<ArticleDTO>>(jsonString.Result);
+                    articlesDto.ForEach(a => articles.Add(new ArticleModel
+                    {
+                        Id = a.ArticleId,
+                        Description = a.ArticleDescription,
+                        Name = a.ArticleName,
+                        Value = a.ArticleValue,
+                        Quantity = 0
+                    }));
+                }
+            }
+
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.PageCount = pageCount;
+
+            return View("Index", articles.Skip(pageIndex * 10).Take(10));
         }
 
         public ActionResult Details(int id)
@@ -49,7 +88,7 @@ namespace Webshop.Controllers
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:61169/");
+                client.BaseAddress = new Uri(System.Configuration.ConfigurationManager.AppSettings["WebApiBaseAddress"]);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetAsync("api/article/" + id).Result;
