363f9b573512221e0504b8de6ea6aa6da4df3f5c
rsemann <semann.rafael@gmail.com>
2016-05-13 01:27:46 -0300

    Grid articles, call WebApi, modal details

--- Webshop/App_Start/BundleConfig.cs
+++ Webshop/App_Start/BundleConfig.cs
@@ -23,9 +23,14 @@ namespace Webshop
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/functions").Include(
+                      "~/Scripts/functions.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = false;
         }
     }
 }
--- Webshop/Controllers/HomeController.cs
+++ Webshop/Controllers/HomeController.cs
@@ -1,29 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Shop.Dto;
+using WebGrease.Css.Extensions;
+using Webshop.Models;
 
 namespace Webshop.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+        {   
             return View();
         }
     }
