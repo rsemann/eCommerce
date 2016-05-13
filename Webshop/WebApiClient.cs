14a98673d02b97311f6da8885677a9cee7d093d6
rsemann <semann.rafael@gmail.com>
2016-05-13 18:00:53 -0300

    Generic classo to access the WebApi

---
+++ Webshop/WebApiClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Webshop
+{
+    public static class WebApiClient<T>
+    {
+        private static string _baseAdress = System.Configuration.ConfigurationManager.AppSettings["WebApiBaseAddress"];
+
+        private static HttpClient GetClient()
+        {
+            var client = new HttpClient {BaseAddress = new Uri(_baseAdress)};
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        public static List<T> GetAll(string uri)
+        {
+            try
+            {
+                using (var client = GetClient())
+                {
+                    var response = client.GetAsync(uri).Result;
+                    response.EnsureSuccessStatusCode();
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    jsonString.Wait();
+                    return JsonConvert.DeserializeObject<List<T>>(jsonString.Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        public static T Get(string uri)
+        {
+            try
+            {
+                using (var client = GetClient())
+                {
+                    var response = client.GetAsync(uri).Result;
+                    response.EnsureSuccessStatusCode();
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    jsonString.Wait();
+                    return JsonConvert.DeserializeObject<T>(jsonString.Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
