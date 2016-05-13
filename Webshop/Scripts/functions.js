a4da42e78d78651907fd43ad224cd45ce91e3eba
rsemann <semann.rafael@gmail.com>
2016-05-13 13:23:57 -0300

    Pagination and toastr

--- Webshop/Scripts/functions.js
+++ Webshop/Scripts/functions.js
@@ -7,28 +7,47 @@ function showModal(self, id) {
         type: "GET",
         cache: false,
         success: function (result) {
+            showalert("oi", "error");
             var div = document.createElement("div");
             document.body.appendChild(div);
             $(div).html(result);
             $(div).modal("show");
         },
         error: function (error) {
-            showalert(error.statusText, "alert-danger");
+            showalert(error.statusText, "error");
         }
     });
 }
 
-function showalert(message, type) {
-
-    $("#showalert")
-        .append('</br><div id="alert" class="alert ' +
-            type +
-            '"><a class="close" data-dismiss="alert">×</a><span>' +
-            message +
-            "</span></div>");
-
-    setTimeout(function () {
-        $("#alert").remove();
-    },
-        5000);
+function addCart(self, id) {
+    showalert("Article addded", "success");
 }
+
+
+function showalert(message, type) {
+    toastr[type](message);
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
+
+$(".nextPage").click(function (evt) {
+    var pageindex = $(evt.target).data("pageindex");
+    $("#pageIndex").val(pageindex);
+    evt.preventDefault();
+    $("form").submit();
+});
