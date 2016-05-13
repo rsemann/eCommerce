ffb53e0d3bd3b3aaeb82c602857db6bb0d9f5e12
rsemann <semann.rafael@gmail.com>
2016-05-13 01:26:47 -0300

    Dependence Injection

---
+++ ShopWebApi/Controllers/ArticleController.cs
@@ -0,0 +1,36 @@
+﻿using Shop.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Shop.Repository.Interfaces;
+
+namespace ShopWebApi.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        private IRepository<ArticleDTO> _repository;
+
+        public ArticleController(IRepository<ArticleDTO> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ArticleDTO> Get()
+        {
+            //string path = HttpContext.Current.ApplicationInstance.Server.MapPath("~/App_Data");
+            return _repository.GetAll();
+        }
+
+        public ArticleDTO Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
