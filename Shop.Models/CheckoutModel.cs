1f0554ccdda19b9b3f045012923ce0a2d817922a
rsemann <semann.rafael@gmail.com>
2016-05-14 14:27:21 -0300

    Models

---
+++ Shop.Models/CheckoutModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public class CheckoutModel
+    {
+        public float SubTotal { get; set; }
+
+        public float TotalVAT { get; set; }
+
+        public float Total { get; set; }
+
+        public List<ArticleModel> Articles = new List<ArticleModel>();
+    }
+}
