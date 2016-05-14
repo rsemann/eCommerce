ecb10388a7c3e59a71d6131c99c5e8749112706f
rsemann <semann.rafael@gmail.com>
2016-05-14 14:29:27 -0300

    Class temporary articles cart

---
+++ ShopBusiness/TemporaryData/CartArticleTemporary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Dto;
+
+namespace ShopBusiness.TemporaryData
+{
+    public class CartArticleTemporary
+    {
+        private static List<ArticleDTO> _cartArticles= new List<ArticleDTO>();
+
+        public void CleanCart()
+        {
+            _cartArticles = new List<ArticleDTO>();
+        }
+
+        public StatusCartDTO AddArticle(ArticleDTO article)
+        {
+            var status = new StatusCartDTO();
+            if (_cartArticles.Exists(a => a.ArticleId == article.ArticleId))
+            {
+                _cartArticles[article.ArticleId] = article;
+                status.Message = "Article was already on the cart. Quantity updated!";
+                status.TypeMessage = "info";
+            }
+            else
+            {
+                _cartArticles.Add(article);
+                status.Message = "Article added to the cart!";
+                status.TypeMessage = "success";
+            }
+
+            status.TotalArticles = _cartArticles.Count;
+            return status;
+        }
+
+        public List<ArticleDTO> GetCartArticles()
+        {
+            return _cartArticles;
+        }
+    }
+}
