9cde17c6b1505846fdd2802545584782a7f7d800
rsemann <semann.rafael@gmail.com>
2016-05-14 14:29:00 -0300

    Cart articles

---
+++ ShopWebApi/Controllers/ArticleCartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Shop.Dto;
+using ShopBusiness.TemporaryData;
+
+namespace ShopWebApi.Controllers
+{
+    public class ArticleCartController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult AddArticleCart(ArticleDTO article)
+        {
+            var message = new CartArticleTemporary().AddArticle(article);
+            return Ok(message);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetCartArticles()
+        {
+            var articles = new CartArticleTemporary().GetCartArticles();
+            return Ok(articles);
+        }
+    }
+}
