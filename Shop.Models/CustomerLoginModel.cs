61bf7f95331651e37a3e62ec8dcedd2e69f2a0cd
rsemann <semann.rafael@gmail.com>
2016-05-16 13:26:15 -0300

    Customer and login

---
+++ Shop.Models/CustomerLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class CustomerLoginModel
+    {
+        [Key]
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid e-mail address")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Shop.Models/CustomerModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class CustomerModel
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [DisplayName("Zip code")]
+        public int ZipCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid e-mail address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
