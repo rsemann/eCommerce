5772ee2ac44b32f4d975da190168f037fed3321d
rsemann <semann.rafael@gmail.com>
2016-05-16 23:22:22 -0300

    Login

---
+++ Shop.Repository/Interfaces/ICustomer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Shop.Models;
+using ShopBusiness.Entities;
+
+namespace Shop.Repository.Interfaces
+{
+    public interface ICustomer
+    {
+        Customer Get(int id);
+        List<Customer> GetMany();
+        Customer Put(Customer customer);
+        Customer ValidateLogin(CustomerLoginModel loginModel);
+    }
+}
---
+++ Shop.Repository/Repositories/CustomerAuth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Models;
+using Shop.Repository.Interfaces;
+using ShopBusiness.DataContext;
+using ShopBusiness.Entities;
+
+namespace Shop.Repository.Repositories
+{
+    public class CustomerAuth : ICustomer
+    {
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetMany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Put(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer ValidateLogin(CustomerLoginModel loginModel)
+        {
+            using (var context = new ShopDataContext())
+            {
+                return context.Customer.FirstOrDefault(c => c.Email == loginModel.Email && c.Password == loginModel.Password);
+            }
+        }
+    }
+}
